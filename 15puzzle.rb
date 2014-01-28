56178965d0cc812edf19db68b285ec4be8bc5455
kmacdonald <kmacdonald@westmont.edu>
2014-01-27 21:21:44 -0800

    is this kind of what we're going for visually?

--- 15puzzle.rb
+++ 15puzzle.rb
@@ -35,8 +35,11 @@ end
 
 class PuzzleWindow < Gosu::Window
   def initialize
-    super(1280, 960, false)
+    super(1280, 700, false)
     self.caption = "Jenni_Puzzle"
+	@background = Gosu::Image.new(self, "background.jpg", true)
+	@scramble_button = Gosu::Image.new(self, "buttons/scramble.tiff", true)
+	@solve_button = Gosu::Image.new(self, "buttons/solve.tiff", true)
     @normal_pics = Gosu::Image.load_tiles(self, "normal face/normal.jpg", -4, -4, true)
 	@perfect_pics = Gosu::Image.load_tiles(self, "perfect face/smaller_perfect.jpg", -4, -4, true)
 	@normal_positions = []
@@ -49,10 +52,14 @@ class PuzzleWindow < Gosu::Window
 	end
     @normal_tiles = []
 	@perfect_tiles = []
-	for i in 0..14
+	for i in 1..15
 		@normal_tiles << Piece.new(self, @normal_pics[i], @normal_positions[i][0], @normal_positions[i][1])
 		@perfect_tiles << Piece.new(self, @perfect_pics[i], @perfect_positions[i][0], @perfect_positions[i][1])
 	end
+  end
+
+  def needs_cursor?
+    true
   end
 
   def update
@@ -60,10 +67,13 @@ class PuzzleWindow < Gosu::Window
   end
 
   def draw
+	@background.draw(0, 0, 0)
     for i in 0..14
       @normal_tiles[i].draw
 	  @perfect_tiles[i].draw
     end
+	@scramble_button.draw(100, 645, 0)
+	@solve_button.draw(350, 645, 0)
   end
 
 end
